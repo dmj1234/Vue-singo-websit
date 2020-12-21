3fd6186d0a9a64f157b2296419a915344e8b170b
灰一样的感觉 <8417614+dmj1234@user.noreply.gitee.com>
2020-12-21 20:40:48 +0800

    deploy

---
+++ precache-manifest.0f9de04241fa886db913b184328b3e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9660f8b0e1ad33c40eb",
+    "url": "/Vue-singo-websit/css/app.6b39ac61.css"
+  },
+  {
+    "revision": "5fdbc454039adf0f42f9633b36ba802d",
+    "url": "/Vue-singo-websit/index.html"
+  },
+  {
+    "revision": "f9660f8b0e1ad33c40eb",
+    "url": "/Vue-singo-websit/js/app.46a69e4e.js"
+  },
+  {
+    "revision": "07dce5b39931f45e3d0b",
+    "url": "/Vue-singo-websit/js/chunk-vendors.9f8cd3a0.js"
+  },
+  {
+    "revision": "79adb7ad9b5e59d1b265abb897ef0e30",
+    "url": "/Vue-singo-websit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-singo-websit/robots.txt"
+  }
+]);
